e04afd63b41004b877edbbd9ce0fa9f8af6dcf91
justin665085 <justin665085@gmail.com>
2023-10-22 14:46:51 +0800

    feat: change base url

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'https://backend-jz5c1g948-justin665085.vercel.app';
+export const BASE_URL = 'https://backend-1tmeiab0y-justin665085.vercel.app';
